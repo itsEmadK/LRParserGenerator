d783685bf0c4474f7996b2960b2a23a54ab4b269
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-05-19 11:29:08 +0330

    Add lraction factory

---
+++ src/lraction.js
@@ -0,0 +1,30 @@
+import './types.js';
+
+/**
+ *
+ * @param {string} type
+ * @param {Set<string>} input
+ * @param {LRState} targetState
+ * @returns {LRAction}
+ */
+export default function createLRAction(type, input, targetState) {
+    return {
+        type,
+        input: new Set(...input),
+        targetState: targetState.clone(),
+        toString(includeInputSymbols) {
+            const inputSymbolsStr = `{${[...this.input].join()}}`;
+            return `${this.type}${this.targetState.number} ${includeInputSymbols ? inputSymbolsStr : ''}`;
+        },
+        clone() {
+            return createLRAction(
+                this.type,
+                new Set([...this.input]),
+                this.targetState.clone(),
+            );
+        },
+        equals(other) {
+            return other.toString() === this.toString();
+        },
+    };
+}
