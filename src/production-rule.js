ba76e7e2ead5d2cd6faea18eb9b4768f01d3f710
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-05-19 09:28:20 +0330

    Add prodrule factory function

---
+++ src/production-rule.js
@@ -0,0 +1,12 @@
+import './types.js';
+
+/**
+ *
+ * @param {number} number
+ * @param {string} LHS
+ * @param {string[]} RHS
+ * @returns {ProductionRule}
+ */
+export default function createProductionRule(number, LHS, RHS) {
+    return { number, LHS: LHS.slice(), RHS: RHS.slice() };
+}
