d9848ac0d49815ba5a03728e44538198b0b1af13
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-05-21 00:34:15 +0330

    Test cases

--- src/index.js
+++ src/index.js
@@ -4,39 +4,24 @@ import LRState from './lrstate.js';
 import Production from './prod.js';
 
 const rules = [
-    new Production('E', ['T', "E'"]),
-    new Production("E'", []),
-    new Production("E'", ['+', 'T', "E'"]),
-    new Production('T', ['F', "T'"]),
-    new Production("T'", []),
-    new Production("T'", ['*', 'F', "T'"]),
-    new Production('F', ['id']),
-    new Production('F', ['(', 'E', ')']),
+    new Production('E', ['L', '=', 'R']),
+    new Production('E', ['R']),
+    new Production('L', ['id']),
+    new Production('L', ['*', 'R']),
+    new Production('R', ['L']),
 ];
 
 const grammar = new Grammar(rules);
 
-console.log(grammar.toString());
-
-console.log(grammar.getFirst(['T', "E'"]));
-
-const item1 = new LRItem(rules[0], 0, ['if', 'id']);
-console.log(item1.hash());
-const item2 = new LRItem(rules[1], 0, []);
-console.log(item2.hash());
-console.log(item1.toString());
-console.log(item2.toString());
-
-console.log(item1.hash());
-console.log(item1.clone().hash());
-
-console.log(item1.hashWithoutLookahead());
-console.log(item1.clone().hashWithoutLookahead());
-
-const augmentedRule = new Production("S'", ['E', '$']);
-const baseItem = new LRItem(augmentedRule, 0);
+// const augmentedRule = new Production("S'", ['E', '$']);
+const augmentedRule = new Production('L', ['*', 'R']);
+const baseItem = new LRItem(augmentedRule, 2, ['=', '$']);
 const state = new LRState([baseItem], grammar);
 console.log(state.hash());
 grammar.rules.forEach(({ num, rule }) => {
     console.log(num, rule.toString());
 });
+
+state.derivedItems.forEach((item) => console.log(item.toString()));
+console.log(state.derivedItems.size);
+console.log(state.toString());
