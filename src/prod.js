9cbb4673b13fc014161ca72952775e033295160c
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-05-20 18:57:48 +0330

    Create Production class

---
+++ src/prod.js
@@ -0,0 +1,44 @@
+export default class Production {
+    /**
+     * @type {string}
+     */
+    #lhs;
+
+    /**
+     * @type {string[]}
+     */
+    #rhs;
+
+    /**
+     *
+     * @param {string} lhs
+     * @param {string[]} rhs
+     */
+    constructor(lhs, rhs) {
+        this.#lhs = lhs;
+        this.#rhs = rhs.slice();
+    }
+
+    /**
+     * @type {string[]} immutable
+     */
+    get rhs() {
+        return this.#rhs.slice();
+    }
+
+    /**
+     * @type {string} immutable
+     */
+    get lhs() {
+        return this.#lhs;
+    }
+
+    hash() {
+        const str = `${this.#lhs}${this.#rhs.join('')}`;
+        return str.split('').map((char) => char.charCodeAt(0));
+    }
+
+    clone() {
+        return new Production(this.#lhs, this.#rhs);
+    }
+}
