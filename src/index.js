7b91432b58e30ac6eff88f1fe1541d4f2c6d7b10
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-05-21 21:37:58 +0330

    Test cases

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import Grammar from './grammar.js';
+import HashSet from './hashset.js';
+import LR1DFA from './lr1dfa.js';
 import LRItem from './lritem.js';
 import LRState from './lrstate.js';
 import Production from './prod.js';
 
-const rules = [
-    new Production('E', ['T', "E'"]),
-    new Production("E'", []),
-    new Production("E'", ['+', 'T', "E'"]),
-    new Production('T', ['F', "T'"]),
-    new Production("T'", []),
-    new Production("T'", ['*', 'F', "T'"]),
-    new Production('F', ['id']),
-    new Production('F', ['(', 'id', ')']),
-];
+// const rules = [
+//     new Production('E', ['T', "E'"]),
+//     new Production("E'", []),
+//     new Production("E'", ['+', 'T', "E'"]),
+//     new Production('T', ['F', "T'"]),
+//     new Production("T'", []),
+//     new Production("T'", ['*', 'F', "T'"]),
+//     new Production('F', ['id']),
+//     new Production('F', ['(', 'E', ')']),
+// ];
+
+// hs.forEach((it) => console.log(it.toString()));
+
 // const rules = [
 //     new Production('E', ['E', '+', 'E']),
 //     new Production('E', ['E', '*', 'E']),
@@ -20,28 +25,27 @@ const rules = [
 //     new Production('E', ['id']),
 //     new Production('E', ['(', 'E', ')']),
 // ];
-// const rules = [
-//     new Production('E', ['L', '=', 'R']),
-//     new Production('E', ['R']),
-//     new Production('L', ['id']),
-//     new Production('L', ['*', 'R']),
-//     new Production('R', ['L']),
-// ];
+const rules = [
+    new Production('E', ['L', '=', 'R']),
+    new Production('E', ['R']),
+    new Production('L', ['id']),
+    new Production('L', ['*', 'R']),
+    new Production('R', ['L']),
+];
 
 const grammar = new Grammar(rules);
 
-const augmentedRule = new Production("S'", ['E', '$']);
-// const augmentedRule = new Production('E', ['L', '=', 'R']);
-const baseItem = new LRItem(augmentedRule, 0, ['$']);
-const state = new LRState([baseItem], grammar);
-console.log(state.hash());
-grammar.rules.forEach(({ num, rule }) => {
-    console.log(num, rule.toString());
+grammar.rules.forEach((rule) => {
+    console.log(rule.toString());
+});
+console.log('==================================\n');
+
+const dfa = new LR1DFA(grammar);
+dfa.states.forEach((s) => {
+    s.baseItems.forEach((item) => {
+        console.log(item.toString());
+    });
+    console.log('-----------------------');
 });
 
-state.derivedItems.forEach((item) => console.log(item.toString()));
-console.log(state.derivedItems.size);
-console.log(state.toString());
-
-console.log(state.goto('T').toString());
-console.log(state.shift('id').toString());
+console.log(dfa.states.size);
