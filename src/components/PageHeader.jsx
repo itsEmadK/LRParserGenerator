119abd47801d7d3d19da85b092d3bd6294bc6679
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-05-29 19:27:57 +0330

    Add and style the page header

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,23 @@
+import styles from './page-heder.module.css';
+
+export default function PageHeader() {
+  return (
+    <div className={styles['container']}>
+      <header className={styles['header']}>
+        <h1>Parser Visualizer</h1>
+        <h3>Enter your context-free grammar to generate the parser</h3>
+        <p>
+          Created by{' '}
+          <span className={styles['name']}>Emad Kheyroddin</span> at Semnan
+          University
+        </p>
+        <p>
+          Inspired by{' '}
+          <a href="https://www.cs.princeton.edu/courses/archive/spring20/cos320/LL1/">
+            Princeton University&apos;s LL1 Parser Visualizer
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
