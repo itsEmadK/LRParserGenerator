06b6fccdde868ac540eb29381880b2fe9e9ebc6a
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-06-07 19:33:33 +0330

    Add state component and its styles

---
+++ src/components/State.jsx
@@ -0,0 +1,41 @@
+import styles from './state.module.css';
+import LRState from '../parse-logic/lrstate.js';
+import Item from './Item.jsx';
+
+/**
+ *
+ * @param {{state:LRState}}
+ */
+export default function State({ state, stateNumber }) {
+  const baseItems = state.baseItems.values().map((bi, index) => {
+    return (
+      <li className={index === 0 ? styles['first'] : ''} key={bi.hash()}>
+        <Item
+          item={bi}
+          isLastBaseItem={
+            index === state.baseItems.size - 1 &&
+            state.derivedItems.size > 0
+          }
+        />
+        {index === 0 && <span>{stateNumber}</span>}
+      </li>
+    );
+  });
+  const derivedItems = state.derivedItems.values().map((di) => {
+    return <Item key={di.hash()} item={di} isLastBaseItem={false} />;
+  });
+  return (
+    <div className={styles['state']}>
+      {[
+        <ul className={styles['base-items']} key={'base'}>
+          {[...baseItems]}
+        </ul>,
+        derivedItems.length > 0 && (
+          <ul className={styles['derived-items']} key={'derived'}>
+            {[...derivedItems]}
+          </ul>
+        ),
+      ]}
+    </div>
+  );
+}
