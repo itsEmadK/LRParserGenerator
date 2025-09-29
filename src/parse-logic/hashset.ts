bfe7004c68f0ffd122f828ba1d5e16f7b0859631
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-09-29 16:35:16 +0330

    Add Hashset class

---
+++ src/parse-logic/hashset.ts
@@ -0,0 +1,61 @@
+import type { Hashable } from './types';
+
+type MapItem<T> = {
+  index: number;
+  item: T;
+};
+
+export default class HashSet<T extends Hashable> {
+  private map = new Map<string, MapItem<T>>();
+  private counter = 0;
+
+  constructor(items: T[]) {
+    items.forEach((item) => {
+      this.add(item);
+    });
+  }
+
+  /**returns true if item.hash() is present in the set. */
+  has(item: T): boolean {
+    const bucket = this.map.get(item.hash());
+    return !!bucket;
+  }
+
+  add(item: T) {
+    if (!this.has(item)) {
+      this.map.set(item.hash(), { item, index: this.counter });
+      this.counter++;
+    }
+  }
+
+  delete(item: T) {
+    this.map.delete(item.hash());
+  }
+
+  clear() {
+    this.map.clear();
+  }
+
+  get values(): T[] {
+    let mapItems: MapItem<T>[] = [];
+    this.map.forEach((mapItem) => {
+      mapItems = [...mapItems, mapItem];
+    });
+
+    const sortedMapItems = mapItems.sort((a, b) =>
+      a.index < b.index ? -1 : 1
+    );
+
+    const items = sortedMapItems.map((mapItem) => mapItem.item);
+    return items;
+  }
+
+  forEach(callback: (item: T) => void) {
+    const items = this.values;
+    items.forEach(callback);
+  }
+
+  get size(): number {
+    return this.values.length;
+  }
+}
