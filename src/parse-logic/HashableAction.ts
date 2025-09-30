f0196466917302442337a1fb94ea5924752fc61a
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-09-30 21:05:12 +0330

    Add HashableAction class

---
+++ src/parse-logic/HashableAction.ts
@@ -0,0 +1,63 @@
+export interface GotoAction {
+  type: 'goto';
+  nonTerminal: string;
+  sourceStateNumber: number;
+  destinationStateNumber: number;
+}
+
+export interface ShiftAction {
+  type: 'shift';
+  terminal: string;
+  sourceStateNumber: number;
+  destinationStateNumber: number;
+}
+
+export interface ReduceAction {
+  type: 'reduce';
+  lookaheadTerminals: Set<string>;
+  ruleNumber: number;
+  sourceStateNumber: number;
+}
+
+export type Action = GotoAction | ShiftAction | ReduceAction;
+
+export default class HashableAction {
+  value: Action;
+  constructor(action: Action) {
+    this.value = action;
+  }
+
+  hash(): string {
+    if (this.value.type === 'goto') {
+      return (
+        this.value.type +
+        ' ' +
+        this.value.nonTerminal +
+        ' ' +
+        this.value.sourceStateNumber +
+        ' ' +
+        this.value.destinationStateNumber
+      );
+    } else if (this.value.type === 'shift') {
+      return (
+        this.value.type +
+        ' ' +
+        this.value.terminal +
+        ' ' +
+        this.value.sourceStateNumber +
+        ' ' +
+        this.value.destinationStateNumber
+      );
+    } else {
+      return (
+        this.value.type +
+        ' ' +
+        this.value.ruleNumber +
+        ' ' +
+        this.value.sourceStateNumber +
+        ' ' +
+        [...this.value.lookaheadTerminals]
+      );
+    }
+  }
+}
--- src/parse-logic/types.ts
+++ src/parse-logic/types.ts
@@ -1,26 +1,3 @@
 export interface Hashable {
   hash(): string;
 }
-
-export type Action = GotoAction | ShiftAction | ReduceAction;
-
-export interface GotoAction {
-  type: 'goto';
-  nonTerminal: string;
-  sourceStateNumber: number;
-  destinationStateNumber: number;
-}
-
-export interface ShiftAction {
-  type: 'shift';
-  terminal: string;
-  sourceStateNumber: number;
-  destinationStateNumber: number;
-}
-
-export interface ReduceAction {
-  type: 'reduce';
-  lookaheadTerminals: Set<string>;
-  ruleNumber: number;
-  sourceStateNumber: number;
-}
