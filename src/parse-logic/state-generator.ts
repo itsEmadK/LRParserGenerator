3746c429702ad907d02a9a0fe6e06d2af4f3b8a6
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-10-01 16:08:25 +0330

    Add StateGenerator class

---
+++ src/parse-logic/state-generator.ts
@@ -0,0 +1,100 @@
+import Grammar from './grammar';
+import GrammarAnalyzer from './grammar-analyzer';
+import HashSet from './hashset';
+import Item from './item';
+import State from './state';
+
+export default class StateGenerator {
+  readonly grammar: Grammar;
+  private readonly grammarAnalyzer: GrammarAnalyzer;
+  constructor(grammar: Grammar) {
+    this.grammar = grammar;
+    this.grammarAnalyzer = new GrammarAnalyzer(grammar);
+  }
+
+  computeDerivedItems(state: State): State {
+    const derivedItems = new HashSet<Item>();
+    while (true) {
+      const oldCount = derivedItems.size;
+
+      [...state.baseItems, ...derivedItems].forEach((item) => {
+        const symbol = item.symbolAfterDot;
+        if (symbol) {
+          if (this.grammar.isNonTerminal(symbol)) {
+            const productions =
+              this.grammar.getProductionsForNonTerminal(symbol);
+            productions.forEach((prod) => {
+              const newItem = new Item(prod, 0);
+              derivedItems.add(newItem);
+            });
+          }
+        }
+      });
+
+      const newCount = derivedItems.size;
+      if (oldCount === newCount) {
+        break;
+      }
+    }
+    return new State(
+      new HashSet([...state.baseItems]),
+      new HashSet([...derivedItems])
+    );
+  }
+
+  computeLookahead(state: State): State {
+    const baseItems = [...state.baseItems];
+    const derivedItems = [...state.derivedItems];
+
+    const calcTerminalCount = () => {
+      let count = 0;
+      derivedItems.forEach((item) => {
+        count += item.lookahead.size;
+      });
+      return count;
+    };
+
+    while (true) {
+      const oldCount = calcTerminalCount();
+
+      for (let i = 0; i < derivedItems.length; i++) {
+        for (let j = 0; j < derivedItems.length + baseItems.length; j++) {
+          const item = [...baseItems, ...derivedItems][j];
+          if (item.symbolAfterDot === derivedItems[i].production.lhs) {
+            const rest = [...item.exprAfterDot];
+            rest.shift();
+            let lookahead = new Set([
+              ...this.grammarAnalyzer.getFirst(rest),
+            ]);
+
+            if (this.grammarAnalyzer.isNullable(rest)) {
+              lookahead = new Set([...lookahead, ...item.lookahead]);
+            }
+            derivedItems[i] = new Item(
+              derivedItems[i].production,
+              derivedItems[i].dotPosition,
+              lookahead
+            );
+          }
+        }
+      }
+
+      const newCount = calcTerminalCount();
+      if (oldCount === newCount) {
+        break;
+      }
+    }
+
+    return new State(
+      new HashSet([...state.baseItems]),
+      new HashSet([...derivedItems])
+    );
+  }
+
+  generate(baseItems: HashSet<Item>): State {
+    let state = new State(new HashSet([...baseItems]), new HashSet());
+    state = this.computeDerivedItems(state);
+    state = this.computeLookahead(state);
+    return state;
+  }
+}
