fd5b96f4da6e35ebd00431a71d26cd2579b1fb69
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-10-01 12:40:08 +0330

    Merge item classes into one

--- src/parse-logic/item.ts
+++ src/parse-logic/item.ts
@@ -4,15 +4,35 @@ import { Hashable } from './types';
 export default class Item implements Hashable {
   readonly production: Production;
   readonly dotPosition: number;
-  constructor(production: Production, dotPosition: number) {
+  readonly lookahead: ReadonlySet<string>;
+
+  constructor(
+    production: Production,
+    dotPosition: number,
+    lookahead: Set<string> = new Set()
+  ) {
     this.production = production;
     this.dotPosition = dotPosition;
     if (dotPosition > production.rhs.length || dotPosition < 0) {
       throw new Error(`invalid dot position(${dotPosition}).`);
     }
+    this.lookahead = lookahead;
   }
 
   hash(): string {
+    const dottedRhs = this.production.rhs.slice();
+    dottedRhs.splice(this.dotPosition, 0, '•');
+    const rhsStr = dottedRhs.join(' ');
+    return (
+      this.production.lhs +
+      ' -> ' +
+      rhsStr +
+      ' , ' +
+      `${[...this.lookahead].join(', ')}`
+    );
+  }
+
+  hashWithoutLookahead(): string {
     const dottedRhs = this.production.rhs.slice();
     dottedRhs.splice(this.dotPosition, 0, '•');
     const rhsStr = dottedRhs.join(' ');
