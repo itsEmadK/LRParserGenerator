aa2e6ef8662278d413409d9d0a262532c2ad6f3e
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-10-02 16:30:03 +0330

    Add method for finding similar states

--- src/parse-logic/dfa.ts
+++ src/parse-logic/dfa.ts
@@ -6,11 +6,7 @@ import type { ParserType } from './types';
 
 class NumberedState extends State {
   readonly stateNumber: number;
-  constructor(
-    type: ParserType,
-    stateNumber: number,
-    state: State
-  ) {
+  constructor(type: ParserType, stateNumber: number, state: State) {
     super(
       type,
       new HashSet([...state.baseItems]),
@@ -79,6 +75,40 @@ export default class DFA {
     return this._states.values.find(
       (state) => state.stateNumber === stateNumber
     );
+  }
+
+  getSimilarLalr1States(): HashSet<State>[] {
+    const visited: Set<number> = new Set();
+    const pools: Set<number>[] = [];
+
+    this.states.forEach((state) => {
+      const pool: Set<number> = new Set();
+      const sameStates = this.states.values.filter(
+        (s) => s.hash(false) === state.hash(false)
+      );
+      sameStates.forEach((s) => {
+        if (!visited.has(s.stateNumber)) {
+          pool.add(s.stateNumber);
+          visited.add(s.stateNumber);
+        }
+      });
+      if (pool.size > 0) {
+        pools.push(pool);
+      }
+    });
+    const statePools: HashSet<State>[] = [];
+    pools.forEach((pool) => {
+      const statePool = new HashSet<State>();
+      pool.forEach((stateNumber) => {
+        const state = this.findStateByNumber(stateNumber);
+        if (state) {
+          statePool.add(state);
+        }
+      });
+      statePools.push(statePool);
+    });
+
+    return statePools;
   }
 
   private getStateNumber(state: State): number {
