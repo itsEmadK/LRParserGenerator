9c052c0e1e0a4a9d609a0d2cf5b511cf2bac87b2
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-10-02 12:12:57 +0330

    Add Transition class

---
+++ src/parse-logic/transition.ts
@@ -0,0 +1,78 @@
+import HashSet from './hashset';
+import type Item from './item';
+import State from './state';
+import type { Hashable } from './types';
+
+export type Transition =
+  | GotoTransition
+  | ShiftTransition
+  | AcceptTransition;
+export class GotoTransition implements Hashable {
+  readonly type = 'goto' as const;
+  readonly source: State;
+  readonly destination: State;
+  readonly originatingItems: HashSet<Item>;
+  readonly nonTerminal: string;
+  constructor(
+    source: State,
+    destination: State,
+    nonTerminal: string,
+    originatingItems: Iterable<Item>
+  ) {
+    this.source = source;
+    this.destination = destination;
+    this.originatingItems = new HashSet([...originatingItems]);
+    this.nonTerminal = nonTerminal;
+  }
+  hash(): string {
+    return (
+      this.type +
+      '\n' +
+      'from: \n' +
+      this.source.hash() +
+      '\n to \n ' +
+      this.destination.hash()
+    );
+  }
+}
+export class ShiftTransition implements Hashable {
+  readonly type = 'shift' as const;
+  readonly source: State;
+  readonly destination: State;
+  readonly terminal: string;
+  readonly originatingItems: HashSet<Item>;
+  constructor(
+    source: State,
+    destination: State,
+    terminal: string,
+    originatingItems: Iterable<Item>
+  ) {
+    this.source = source;
+    this.destination = destination;
+    this.terminal = terminal;
+    this.originatingItems = new HashSet([...originatingItems]);
+  }
+  hash(): string {
+    return (
+      this.type +
+      '\n' +
+      'from: \n' +
+      this.source.hash() +
+      '\n to \n ' +
+      this.destination.hash()
+    );
+  }
+}
+
+export class AcceptTransition implements Hashable {
+  readonly type = 'accept' as const;
+  readonly source: State;
+  readonly originatingItems: HashSet<Item>;
+  constructor(source: State, originatingItems: Iterable<Item>) {
+    this.source = source;
+    this.originatingItems = new HashSet([...originatingItems]);
+  }
+  hash(): string {
+    return this.type + '\n' + this.source.hash();
+  }
+}
