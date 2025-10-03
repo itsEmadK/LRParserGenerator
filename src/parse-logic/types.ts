ead58953a5b6397d2f2cdbda8ca38d80a7ae0570
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-10-03 15:39:54 +0330

    Add required types

--- src/parse-logic/types.ts
+++ src/parse-logic/types.ts
@@ -3,3 +3,41 @@ export interface Hashable {
 }
 
 export type ParserType = 'lr1' | 'lalr1' | 'slr1' | 'lr0';
+
+export type Action =
+  | ReduceAction
+  | GotoAction
+  | ShiftAction
+  | AcceptAction;
+
+export type ReduceAction = {
+  type: 'reduce';
+  ruleNumber: number;
+};
+export type ShiftAction = {
+  type: 'shift';
+  destination: number;
+};
+export type GotoAction = {
+  type: 'goto';
+  destination: number;
+};
+export type AcceptAction = {
+  type: 'accept';
+};
+export type ParseTableCell = Action | Array<Action> | undefined;
+export type ReadonlyParseTableCell =
+  | Readonly<Action>
+  | ReadonlyArray<Readonly<Action>>
+  | undefined;
+
+export type ParseTable = {
+  [stateNumber: number]: {
+    [symbol: string]: ParseTableCell;
+  };
+};
+export type ReadonlyParseTable = {
+  readonly [stateNumber: number]: {
+    readonly [symbol: string]: ReadonlyParseTableCell;
+  };
+};
