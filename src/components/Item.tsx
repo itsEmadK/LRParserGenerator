fce3d4c437cba283c27f75bf3a40d70091291fa5
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-10-21 20:01:09 +0330

    Rewrite item component with ts

---
+++ src/components/Item.tsx
@@ -0,0 +1,24 @@
+import type Item from '../dfa/item';
+import styles from '../styles/item.module.css';
+
+type LrItemProps = {
+  item: Item;
+};
+
+export default function LrItem({ item }: LrItemProps) {
+  const dottedRhs = [...item.production.rhs];
+  dottedRhs.splice(item.dotPosition, 0, '•');
+  return (
+    <div className={`${styles['item-container']}`}>
+      <div className={`${styles['item']} }`}>
+        <p className={styles['lhs']}>{item.production.lhs}</p>
+        <p className={styles['arrow']}>{' -> '}</p>
+        <p className={styles['rhs']}>{dottedRhs.join(' ')}</p>
+        <p className={styles['lookahead']}>
+          {(item.lookahead ?? { size: 0 }).size > 0 && ' ,'}
+          {`{${[...(item.lookahead ?? [])].join(', ')}}`}
+        </p>
+      </div>
+    </div>
+  );
+}
