c3b9f671413f83f27bed3cc0bcd5251715ff336c
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-10-22 11:05:38 +0330

    Add AutomataSection

---
+++ src/components/AutomataSection.tsx
@@ -0,0 +1,32 @@
+import styles from '../styles/automata-section.module.css';
+import { useDfa } from '../contexts/AppContext';
+import StateWithTransitions from './StateWithTransitions';
+
+export default function AutomataSection() {
+  const dfa = useDfa();
+
+  return (
+    <section className={styles['automata']}>
+      <h2>Handle Finding Automata States:</h2>
+      <ul className={styles['tips']}>
+        <li>
+          * Click on a circle to scroll into its corresponding state.
+        </li>
+        <li>* Hover on a circle to reveal its corresponding state.</li>
+      </ul>
+      <ul className={styles['states']}>
+        {[...dfa.states].map((state, index) => {
+          return (
+            <StateWithTransitions
+              key={index}
+              state={state}
+              transitions={[
+                ...dfa.getStateOutwardTransitions(state.stateNumber),
+              ]}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
